caad067bfe4d8c63307b4578e729d4360648ebe6
jakovnovak30 <jakovnovak30@gmail.com>
2025-09-13 20:30:33 +0200

    fix q3

--- q3/CSA_8.sv
+++ q3/CSA_8.sv
@@ -4,22 +4,29 @@ module CSA_8(
     output logic carry
   );
 
-  logic [3:0] sum1, sum2;
-  logic carry1;
+  logic [3:0] sum1, sum2_1, sum2_0;
+  logic c_3, c_7_1, c_7_0;
   adder_4 adder1(
       .A(A[3:0]), .B(B[3:0]),
       .carry_in(0),
       .sum(sum1),
-      .carry(carry1)
+      .carry(c_3)
     );
 
-  adder_4 adder2(
+  adder_4 adder2_0(
       .A(A[7:4]), .B(B[7:4]),
-      .carry_in(carry1),
-      .sum(sum2),
-      .carry(carry)
+      .carry_in(0),
+      .sum(sum2_0),
+      .carry(c_7_0)
+    );
+  adder_4 adder2_1(
+      .A(A[7:4]), .B(B[7:4]),
+      .carry_in(1),
+      .sum(sum2_1),
+      .carry(c_7_1)
     );
 
-  assign sum = {sum2, sum1};
+  assign sum = {c_3 ? sum2_1 : sum2_0, sum1};
+  assign carry = c_3 ? c_7_1 : c_7_0;
 
 endmodule
--- q3/CSA_8_tb.sv
+++ q3/CSA_8_tb.sv
@@ -1,7 +1,7 @@
 module CSA_8_tb();
 
-logic [7:0] A = 0, B = 0;
-logic [7:0] sum;
+logic unsigned [7:0] A = 0, B = 0;
+logic unsigned [7:0] sum;
 logic carry;
 
 CSA_8 UUT(
@@ -12,13 +12,16 @@ CSA_8 UUT(
 initial begin
   #10;
   A = 5; B = 6;
-  #10;
+  #10 assert (A + B == sum && carry == 0);
   A = 10; B = 6;
-  #10;
+  #10 assert (A + B == sum && carry == 0);
   A = 160; B = 150; // overflow
-  #10;
+  #10 assert (A + B == sum && carry == 1);
   A = 100; B = 120;
+  #10 assert (A + B == sum && carry == 0);
   #10;
+  A = 8'b10001111; B = 8'b01110001;
+  #10 assert (A + B == sum && carry == 1);
 end
 
 endmodule
